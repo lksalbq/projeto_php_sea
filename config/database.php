c5a933bcb8e6a5ffb1100f15adfb51fd6f2c8b56
Lucas de Albuquerque <lks.albq@gmail.com>
2016-01-15 20:04:53 -0200

    CRUDE projeto SEA

--- config/database.php
+++ config/database.php
@@ -1,40 +1,29 @@
 <?php
 
 class Database {
-  
+
     public $conn;
-    private $url;
-    private $host;
-    private $db_name;
-    private $username;
-    private $password;
-    
-    public function getURL(){
-        if(getenv("CLEARDB_DATABASE_URL")){
-           $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-           $host = $url["host"];
-           $db_name = substr($url["path"], 1);
-           $username = $url["user"];
-           $password = $url["pass"];
-           
-        }else{
+
+    public function getConnection() {
+        if (getenv("CLEARDB_DATABASE_URL")) {
+            $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+            $host = $url["host"];
+            $db_name = substr($url["path"], 1);
+            $username = $url["user"];
+            $password = $url["pass"];
+
+            $this->conn = null;
+
+            try {
+                $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            } catch (PDOException $exception) {
+                echo "Connection error: " . $exception->getMessage();
+            }
+
+            return $this->conn;
+        } else {
             die('Erro ao estabelecer conexão com o banco de dados!');
         }
-        
-    }
-    
-    public function getConnection() {
-        getURL();
-        
-        $this->conn = null;
-
-        try {
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-        } catch (PDOException $exception) {
-            echo "Connection error: " . $exception->getMessage();
-        }
-
-        return $this->conn;
     }
 
 }
