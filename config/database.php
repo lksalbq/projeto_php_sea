9abff138af027635b5136f6d2c68047700b6e10d
Lucas de Albuquerque <lks.albq@gmail.com>
2016-01-18 15:18:33 -0200

    CRUDE projeto SEA

--- config/database.php
+++ config/database.php
@@ -2,14 +2,17 @@
 
 class Database {
 
-    
-    private $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    private $url;
+    //private $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
     private $host = $url["host"];
     private $db_name = substr($url["path"], 1);
     private $username = $url["user"];
     private $password = $url["pass"];
     public $conn;
 
+    public function __construct(){
+        $this->url =  parse_url(getenv("CLEARDB_DATABASE_URL"));
+    }
     
     public function getConnection() {
         
